943850f05a3953e8a18360063885be9ca62a281c
Bantyb792 <bantyb792@gmail.com>
2024-07-17 11:07:29 +0530

    error solved

--- schema.js
+++ schema.js
@@ -17,5 +17,5 @@ module.exports.reviewSchema=joi.object({
     review: joi.object({
         rating:joi.number().required().min(1).max(5),
         comment: joi.string().required()
-    }).required
+    }).required()
 });
