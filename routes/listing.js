855e0af182e944021ce23cbea4ac23c7d45f3bbd
Bantyb792 <bantyb792@gmail.com>
2024-07-19 11:52:46 +0530

    flash success includes

--- routes/listing.js
+++ routes/listing.js
@@ -38,7 +38,7 @@ router.get("/:id", wrapAsync (async (req,res)=>{
 router.post("/",validateListing, wrapAsync(async (req,res)=>{
 const newListing= new Listing(req.body.listing);
 await newListing.save();
-req.flash("success","New Listing Created!")
+req.flash("success","New Listing Created!");
 res.redirect("/listings");
 }));
 
@@ -54,6 +54,7 @@ res.render("listings/edit.ejs",{listing});
 router.put("/:id", validateListing, wrapAsync( async (req,res)=>{
 let {id}=req.params;
 await Listing.findByIdAndUpdate(id,{...req.body.listing});
+req.flash("success","Listing Upadated!");
 res.redirect(`/listings/${id}`);
 }));
 
@@ -61,6 +62,7 @@ res.redirect(`/listings/${id}`);
 router.delete("/:id", wrapAsync( async(req,res)=>{
 let {id}=req.params;
 let deletedListing= await Listing.findByIdAndDelete(id);
+req.flash("success","Listing Deleted!");
 console.log(deletedListing);
 res.redirect("/listings");
 }));
