a96243e506e87b0a8d3158c31319725a8f46fdbb
Bantyb792 <bantyb792@gmail.com>
2024-07-20 17:49:11 +0530

    user register data saving

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,23 @@
 const express= require("express");
 const router =express.Router();
+const User= require("../models/user.js");
 
 router.get("/signup",(req,res)=>{
     res.render("../views/users/signup.ejs");
+});
+
+router.post("/signup", async (req,res)=>{
+    try{
+        let{username,email,password} = req.body;
+    const newUser= new User ({email,username});
+    const registerUser = await User.register(newUser,password);
+    console.log(registerUser);
+    req.flash("success","Welcome to Wanderust");
+    res.redirect("/listings");
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
 });
 
 module.exports=router;
