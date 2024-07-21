f45dc1ea068df276ac6c24f914c2e628c402835f
Bantyb792 <bantyb792@gmail.com>
2024-07-21 17:20:59 +0530

    auto login after signup

--- routes/user.js
+++ routes/user.js
@@ -11,10 +11,15 @@ router.post("/signup", async (req,res)=>{
     try{
         let{username,email,password} = req.body;
     const newUser= new User ({email,username});
-    const registerUser = await User.register(newUser,password);
-    console.log(registerUser);
-    req.flash("success","Welcome to Wanderust");
-    res.redirect("/listings");
+    const registerdUser = await User.register(newUser,password);
+    console.log(registerdUser);
+    req.login(registerdUser,(err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","Welcome to Wanderust");
+        res.redirect("/listings");
+    })
     }catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
